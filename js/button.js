20b601a113052169570fbfe4d7ce23d529136a86
MillionDream <milliondream@qq.com>
2024-01-02 17:59:24 +0800

    ✨ feat: Add Scroll Button.

--- js/button.js
+++ js/button.js
@@ -1,4 +1,4 @@
-function createButton(innerText, top, selector, clickMessage, clickAction) {
+function createButton(innerText, top, selector, clickMessage, isScroll) {
   let button = document.createElement("button");
   button.innerText = innerText;
   button.style.position = "fixed";
@@ -11,7 +11,11 @@ function createButton(innerText, top, selector, clickMessage, clickAction) {
   button.addEventListener("click", function () {
     let element = document.querySelector(selector);
     if (element) {
-      element.click();
+      if (isScroll) {
+        element.scrollIntoView(false);
+      } else {
+        element.click();
+      }
     } else {
       alert(clickMessage);
     }
@@ -24,19 +28,26 @@ let editButton = createButton(
   80,
   "#viewthread_foot\\ cl > div > div > em > a.times_editp",
   "未找到编辑按钮！",
-  null
+  false
 );
 let beforePageButton = createButton(
   "上一页",
   130,
   "#ct > div.cl > div.mn > div.box.cl > div.pgs.mtm.mbm.cl > div > a.prev",
   "未找到按钮或已经是第一页了！",
-  null
+  false
 );
 let nextPageButton = createButton(
   "下一页",
   180,
   "#ct > div.cl > div.mn > div.box.cl > div.pgs.mtm.mbm.cl > div > a.nxt",
   "未找到按钮或已经是最后一页了！",
-  null
+  false
+);
+let editArea = createButton(
+  "跳转回复",
+  230,
+  "#fastpostform > table > tbody > tr > td",
+  "未找到按钮！",
+  true
 );
